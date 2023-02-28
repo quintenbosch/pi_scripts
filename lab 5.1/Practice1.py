a78814fb8998d06240f5d7446e2e658248332ef2
quintenbosch <quintenbosch@hotmail.com>
2023-02-28 16:01:26 +0000

    changed Practice1.py

--- lab 5.1/Practice1.py
+++ lab 5.1/Practice1.py
@@ -3,15 +3,19 @@ import time
 # to use raspberry pi GPIO numbers
 GPIO.setmode(GPIO.BCM)
 
+def blink(pin):
+  # setup GPIO output channel
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 1)
+  time.sleep(0.5)
+  GPIO.output(pin, 0)
+  time.sleep(0.5)
+
 try:
   while True:
 # GPIO pin 1
-    GPIO.setup(1, GPIO.OUT)
-    GPIO.output(1, 1)
-    time.sleep(1)
-    GPIO.output(1, 0)
-    time.sleep(1)
+    blink(1)
 
 except KeyboardInterrupt:
-  print("Interrupted")
+  print(" Interrupted")
   GPIO.cleanup()
