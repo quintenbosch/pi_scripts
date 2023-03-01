58f073759997fb6fe2865d25e0df4c4c28063df6
quintenbosch <quintenbosch@hotmail.com>
2023-03-01 18:12:05 +0000

    Practice3.py

---
+++ lab 5.1/Practice3.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+# to use raspberry pi GPIO numbers
+GPIO.setmode(GPIO.BCM)
+
+def blink(pin1, pin2, pin3, pin4):
+  # setup GPIO output channel
+  GPIO.setup(pin1, GPIO.OUT)
+  GPIO.setup(pin2, GPIO.OUT)
+  GPIO.setup(pin3, GPIO.OUT)
+  GPIO.setup(pin4, GPIO.OUT)
+  GPIO.output(pin1, 1)
+  GPIO.output(pin1, 0)
+  GPIO.output(pin2, 1)
+  GPIO.output(pin2, 0)
+  GPIO.output(pin3, 1)
+  GPIO.output(pin3, 0)
+  GPIO.output(pin4, 1)
+  GPIO.output(pin4, 0)
+
+# main program blink GPIO18(pin12) 10 times
+for i in range(0,10):
+  blink(17, 27, 22, 23)
+
+# cleanup
+GPIO.cleanup()
+print("Program executed!")
