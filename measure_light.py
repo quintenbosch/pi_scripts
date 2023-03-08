76c2aacee9124be1ebc318a272c998e419398cb3
quintenbosch <quintenbosch@hotmail.com>
2023-03-08 17:23:21 +0000

    change measure_light.py

--- measure_light.py
+++ measure_light.py
@@ -11,12 +11,12 @@ try:
         time.sleep(0.1)
 
         GPIO.setup(18, GPIO.IN)
-        starttime = datetime.datetime.now()
+        starttime = time.time()
         while(GPIO.input(18)==0):
             pass
-        stoptime = datetime.datetime.now()
+        stoptime = time.time()
         interval = (stoptime - starttime)
-        print(interval.microseconds)
+        print(round(interval*1000000))
 
 except KeyboardInterrupt:
     GPIO.cleanup()
